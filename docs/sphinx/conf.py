a9e09c61ec1eaa6ddc0d7923d598f3f4af2ac0ec
Joel Lefkowitz <joel.lefkowitz@teya.co>
2024-04-24 10:57:35 +0100

    Add theme variables

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -6,9 +6,6 @@ from glob import glob
 project = "Caches"
 version = "0.1.0"
 
-primary = "#4165FF"
-secondary = "#914FF5"
-
 project_copyright = f"{date.today().year} Joel Lefkowitz"
 
 extensions = [
@@ -28,10 +25,18 @@ breathe_projects = {
 
 breathe_default_members = (
     "members",
-    "protected-members",
     "private-members",
+    "protected-members",
     "undoc-members",
 )
+
+theme = {
+    "color-problematic": "#4165FF",
+    "color-brand-content": "#4165FF",
+    "color-brand-primary": "#914FF5",
+    "font-stack": "Space Grotesk, sans-serif",
+    "font-stack--monospace": "CQ Mono, monospace",
+}
 
 html_theme = "furo"
 html_title = project
@@ -39,18 +44,12 @@ html_static_path = [""]
 html_js_files = ["scripts.js"]
 html_css_files = ["styles.css"]
 html_theme_options = {
-    "font-stack": "Space Grotesk, sans-serif",
-    "font-stack--monospace": "CQ Mono, monospace",
     "light_css_variables": {
-        "color-brand-content": primary,
-        "color-problematic": primary,
-        "color-brand-primary": secondary,
+        **theme,
         "color-highlight-on-target": "#E0E0E0",
     },
     "dark_css_variables": {
-        "color-problematic": primary,
-        "color-brand-content": primary,
-        "color-brand-primary": secondary,
+        **theme,
         "color-highlight-on-target": "#202020",
     },
 }
