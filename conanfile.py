6a9d4bfcc82f38c4b67c1a2e37e0301b870467c4
Joel Lefkowitz <joel.lefkowitz@teya.com>
2024-12-01 15:49:10 +0000

    Bump version: 0.1.0 → 0.2.0

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class CachesConan(ConanFile):
     name = "caches"
     description = "Extensible cache templates."
 
-    version = "0.1.0"
+    version = "0.2.0"
     license = "MIT"
 
     url = "https://github.com/conan-io/conan-center-index"
